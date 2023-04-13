684fb669f88348a904386d2a2ce57c77cd634a83
BrendaGilisho <b.gilisho@alustudent.com>
2023-04-13 21:05:46 +0200

    starter

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -7,14 +7,14 @@ const test = (
     <div>
         <h1 className="text-4xl text-black-500">Hello World</h1>
         <p>You got this babes!!</p>
+        
 
-        <button id="button_custom" className="bg-white px-8 py-2 rounded-3xl font-semibold text-lg text-black">login</button>
-
-
- 
-
+        <button id="button_custom" className="bg-white px-8 py-2 rounded-3xl font-semibold text-lg text-black">Similar Products</button>
+        <button id="button_custom" className="bg-white px-8 py-2 rounded-3xl font-semibold text-lg text-black">Top RW E-Commerce Websites</button>
     </div>
 )
+
+
 
 const container = document.createElement('div')
 document.body.appendChild(container)
